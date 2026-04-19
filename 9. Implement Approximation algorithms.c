103f0a2c63895c3509af5ceda8a63f17647c4037
11249a389-ui <11249a389@kanchiuniv.ac.in>
2026-04-19 21:20:15 +0530

    approximation algorithm for Vertex Cover
    Implement an approximation algorithm for Vertex Cover.

---
+++ 9. Implement Approximation algorithms.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    int e;
+    int edges[100][2];
+    int selected[100] = {0};
+
+    printf("Enter number of edges: ");
+    scanf("%d", &e);
+
+    printf("Enter edges:\n");
+    for (int i = 0; i < e; i++) {
+        scanf("%d %d", &edges[i][0], &edges[i][1]);
+    }
+
+    printf("Approximate Vertex Cover: ");
+    for (int i = 0; i < e; i++) {
+        if (!selected[i]) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            printf("%d %d ", u, v);
+
+            for (int j = i; j < e; j++) {
+                if (edges[j][0] == u || edges[j][1] == u ||
+                    edges[j][0] == v || edges[j][1] == v) {
+                    selected[j] = 1;
+                }
+            }
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
